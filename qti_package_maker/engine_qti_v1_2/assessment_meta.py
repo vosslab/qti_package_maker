0ff7feb2abe17426db18f106193c0fcc56325153
Neil Voss <vossman77@yahoo.com>
2025-02-15 03:02:39 -0600

    building out the content makers

---
+++ qti_package_maker/engine_qti_v1_2/assessment_meta.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import os
+from lxml import etree
+
+#==============
+def generate_assessment_meta(package_name: str) -> etree.ElementTree:
+	"""
+	Generates the XML for the question bank (assessment_meta.xml).
+
+	Args:
+		package_name (str): The title for the set of assessments.
+
+	Returns:
+		etree.ElementTree: The generated XML tree for assessment_meta.xml.
+	"""
+	# Define namespaces
+	nsmap = {
+		None: "http://canvas.instructure.com/xsd/cccv1p0",
+		"xsi": "http://www.w3.org/2001/XMLSchema-instance"
+	}
+
+	# Create the root element <quiz> with namespaces
+	quiz = etree.Element("quiz", nsmap=nsmap)
+
+	# Add the xsi:schemaLocation attribute
+	quiz.attrib["{http://www.w3.org/2001/XMLSchema-instance}schemaLocation"] = (
+		"http://canvas.instructure.com/xsd/cccv1p0 "
+		"https://canvas.instructure.com/xsd/cccv1p0.xsd"
+	)
+
+	# Add identifier attribute
+	quiz.set("identifier", "assessment_meta")
+
+	# Add <title>
+	title_element = etree.SubElement(quiz, "title")
+	title_element.text = package_name
+
+	# Add <assignment>
+	assignment_element = etree.SubElement(quiz, "assignment", identifier="assignment_name")
+
+	# Add <title> inside <assignment>
+	assignment_title = etree.SubElement(assignment_element, "title")
+	assignment_title.text = package_name
+
+	return etree.ElementTree(quiz)
+
+#==============
+def dummy_test_run():
+	"""
+	Run a test generation of assessment_meta.xml with a sample package name.
+	"""
+	package_name = "Questions about Blah Blah Blah"
+	assessment_meta_etree = generate_assessment_meta(package_name)
+
+	# Write to file
+	assessment_meta_xml_string = etree.tostring(
+		assessment_meta_etree, pretty_print=True, xml_declaration=True, encoding="UTF-8"
+	)
+	assessment_meta_path = "assessment_meta.xml"
+	with open(assessment_meta_path, "w", encoding="utf-8") as f:
+		f.write(assessment_meta_xml_string.decode("utf-8"))
+
+#==============
+if __name__ == "__main__":
+	dummy_test_run()
