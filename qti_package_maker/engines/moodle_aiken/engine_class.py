4f9be142b9ead207334eedbf51ad7a0318ade9bd
Neil Voss <vosslab@NVOSS-MAC02.local>
2025-10-23 10:58:12 -0500

    add moodle aiken format

---
+++ qti_package_maker/engines/moodle_aiken/engine_class.py
@@ -0,0 +1,37 @@
+
+# Standard Library
+
+# Pip3 Library
+
+# QTI Package Maker
+from qti_package_maker.engines import base_engine
+from qti_package_maker.engines.moodle_aiken import write_item
+
+class EngineClass(base_engine.BaseEngine):
+	def __init__(self, package_name: str, verbose: bool=False):
+		# Call the base engine constructor
+		super().__init__(package_name, verbose)
+		# set the write_item module (required)
+		self.write_item = write_item
+		# Verify that the correct write_item module is imported
+		self.validate_write_item_module()
+
+	#==============
+	def read_package(self, infile: str):
+		raise NotImplementedError
+
+	#==============
+	def save_package(self, item_bank, outfile: str = None):
+		outfile = self.get_outfile_name('aiken', 'txt', outfile)
+		assessment_items_tree = self.process_item_bank(item_bank)
+		if len(assessment_items_tree) == 0:
+			return None
+		# Write assessment items to the file
+		with open(outfile, "w") as f:
+			count = 0
+			for item_num, assessment_text in enumerate(assessment_items_tree, start=1):
+				f.write(assessment_text)
+				count += 1
+		if self.verbose is True:
+			print(f"Saved {count} assessment items to {outfile}")
+		return outfile
---
+++ qti_package_maker/engines/moodle_aiken/write_item.py
@@ -0,0 +1,89 @@
+ENGINE_NAME = "moodle_aiken"
+
+from qti_package_maker.common import string_functions
+
+#==============================================================
+def is_valid_content(content_text):
+	#override for now
+	return True
+
+	if '<mathml' in content_text.lower():
+		#print("problem contains mathml")
+		return False
+	if 'rdkit' in content_text.lower():
+		#print("problem contains rdkit")
+		return False
+	if '<table' in content_text.lower():
+		#print("problem contains a table")
+		return False
+	return True
+
+#====================
+def is_valid_list(list_of_strings):
+	for content_text in list_of_strings:
+		if not is_valid_content(content_text):
+			return False
+	return True
+
+#==============================================================
+def MC(item_cls):
+	#item_number: int, crc16_text: str, question_text: str, choices_list: list, answer_text: str):
+	"""Create a Multiple Choice (Single Answer; Radio Buttons) question."""
+	local_question_text = string_functions.make_question_pretty(item_cls.question_text)
+	if not is_valid_content(local_question_text):
+		return None
+	if not is_valid_list(item_cls.choices_list):
+		return None
+	assessment_text = ''
+	assessment_text += item_cls.question_text
+	assessment_text += '\n'
+	already_has_prefix = string_functions.has_prefix(item_cls.choices_list)
+	for i, choice_text in enumerate(item_cls.choices_list):
+		local_choice_text = choice_text
+		if already_has_prefix:
+			local_choice_text = string_functions.strip_prefix_from_string(local_choice_text)
+		letter_prefix = string_functions.number_to_letter(i+1)
+		assessment_text += f"{letter_prefix}. {local_choice_text}\n"
+	answer_letter = string_functions.number_to_letter(item_cls.answer_index+1)
+	assessment_text += f"ANSWER: {answer_letter}\n"
+	assessment_text += '\n\n'
+	return assessment_text
+
+#==============================================================
+def MA(item_cls):
+	#item_number: int, crc16_text: str, question_text: str, choices_list: list, answers_list: list):
+	"""Create a Multiple Answer (Checkboxes) question."""
+	return None
+
+#==============================================================
+def MATCH(item_cls):
+	#item_number: int, crc16_text: str, question_text: str, prompts_list: list, choices_list: list):
+	"""Create a Matching question where users match items from two lists."""
+	#MAT TAB question text TAB answer text TAB matching text TAB answer two text TAB matching two text
+	return None
+
+#==============================================================
+def NUM(item_cls):
+	#item_number: int, crc16_text: str,
+	#question_text: str, answer_float: float, tolerance_float: float, tolerance_message=True):
+	"""Create a Numerical question with an accepted tolerance range."""
+	return None
+
+#==============================================================
+def FIB(item_cls):
+	#item_number: int, crc16_text: str, question_text: str, answers_list: list):
+	"""Create a Fill-in-the-Blank (Single Blank) question."""
+	return None
+
+#==============================================================
+# Create a Fill-in-the-Blank (Multiple Blanks) question using answer mapping.
+def MULTI_FIB(item_cls):
+	#item_number: int, crc16_text: str, question_text: str, answer_map: dict) -> str:
+	"""Create a Fill-in-the-Blank (Multiple Blanks) question using answer mapping."""
+	return None
+
+#==============================================================
+def ORDER(item_cls):
+	#item_number: int, crc16_text: str, question_text: str, ordered_answers_list: list):
+	"""Create an Ordered List question where users arrange items in a correct sequence."""
+	return None
